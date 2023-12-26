5b9178bca0dc7e77687917af42f7707302835684
YYW <y2w9267@gmail.com>
2023-12-26 15:13:05 +0900

    no message

---
+++ goyangEE/Assets/01_Scripts/AreaScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AreaScript : MonoBehaviour
+{
+    public RectTransform AreaRect, BoxRect, TextRect;
+    public GameObject Tail;
+    public TextMeshProUGUI TimeText, UserText, DateText;
+    public Image UserImage;
+    public string Time, User;
+}
